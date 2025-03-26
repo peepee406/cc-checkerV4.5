8ea26cd8ba9f190e163f00fe81cea070d3b7ae99
peepee406 <71884583+peepee406@users.noreply.github.com>
2025-03-27 00:03:17 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'telegram_bot_token' => 'YOUR_BOT_TOKEN_HERE', // Replace with token from BotFather
+    'mode_proxy' => 'off',
+    'proxy_list' => 'proxy.txt',
+    'proxy_type' => 'http'
+];
